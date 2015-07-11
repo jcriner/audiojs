fad87d44c28ea68774fa8f9228af6e956ba5b4d0
Jacob Criner <jcriner@jcriner-macbookpro.roam.corp.google.com>
2015-07-10 20:21:59 -0700

    Added simple Noisemaker API.
    HTML updated to include a simple page with start, stop, and mute buttons for a single note tone.

--- audio.js
+++ audio.js
@@ -1,10 +1,44 @@
-// Currently, this thing is so stupid it just plays 440HZ on load.
+// Simple Noisemaker object for producing a 440HZ 'A' note. This sound
+// is mutable, and stoppable. It can currently only be started once,
+// so mute should be used to start and stop the noise.
 
-// Dumb script:
-var context = new AudioContext();
-var osc = context.createOscillator();
-osc.connect(context.destination);
-osc.start(0);  // 0 is the number of seconds before sound plays
+function Noisemaker() {
+    this.context = new AudioContext();
+    this.gainNode = this.context.createGain();
+    this.oscillator = this.context.createOscillator();
+    this.oscillator.connect(this.gainNode);
+    this.gainNode.connect(this.context.destination);
+
+    this.lastVolumeSetting = 1;
+}
+
+Noisemaker.prototype.start = function() {
+    this.oscillator.start(0);
+}
+
+Noisemaker.prototype.stop = function() {
+    this.oscillator.stop();
+}
+
+Noisemaker.prototype.isMuted = function() {
+    return this.isMuted;
+}
+
+Noisemaker.prototype.mute = function() {
+    this.lastVolumeSetting = this.gainNode.gain.value;
+    this.gainNode.gain.value = 0;
+}
+
+Noisemaker.prototype.unmute = function() {
+    this.gainNode.gain.value = this.lastVolumeSetting;
+    this.isMuted = false;
+}
+
+
+// All these things are pluggable. If you want to control the volume,
+// you create a GainNode, and 'connect' the oscillator into the
+// GainNode. The GainNode can then be hooked up to the context
+// destination (e.g. speakers).
 
 // TODO:
 //   * vary frequency over time
